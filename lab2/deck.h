a74d920ab6f9745a334361d171d3e49a1f747175
rgsmith88 <rgsmith@wustl.edu>
2016-11-15 20:56:10 -0600

    Update deck.h

--- lab2/deck.h
+++ lab2/deck.h
@@ -1,1 +1,32 @@
 
+#ifndef deck_h
+#define deck_h
+#include "stdafx.h"
+#include "functions.h"
+#include "hand.h"
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Hand;
+
+class Deck {
+	public:
+		Deck(char * filename);
+		// Can use virtual destructor because there is no special destructing that needs to be done here
+		int load(char * filename);
+		int shuffle();
+		int size() const;
+		friend void operator<< (Hand& hand, Deck& deck);
+		friend ostream& operator<< (ostream& o, const Deck& deck);
+	private:
+		vector<Card> deck_vector;
+};
+
+ostream& operator<< (ostream& o, const Deck& deck);
+void operator<< (Hand& hand, Deck& deck); //void because doesn't need to return anything
+int handRankInt(char* handRank);
+bool poker_rank(const Hand& hand1, const Hand& hand2);
+
+#endif;
