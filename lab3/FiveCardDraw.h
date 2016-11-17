0b1270169cf5af9de6db50f06a90296406f8cb17
rgsmith88 <rgsmith@wustl.edu>
2016-11-17 12:58:50 -0600

    Create FiveCardDraw.h

---
+++ lab3/FiveCardDraw.h
@@ -0,0 +1,29 @@
+#pragma once
+//#include "Game.h"
+#pragma once
+#pragma once
+#include "stdafx.h"
+#include "functions.h"
+#include "deck.h"
+#include "hand.h"
+#include "player.h"
+#include "Game.h"
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+
+class FiveCardDraw: public Game {
+public:
+
+	FiveCardDraw();
+		
+
+private:
+	size_t dealer; //start at 0
+	Deck deck; //under the assumption that the deck constructor initializes empty deck
+	int before_turn(player & p); //should be virtual
+
+};
