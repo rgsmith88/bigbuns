0a3d2bb27d1bd33f5ae1b695d4f349978983652e
rgsmith88 <rgsmith@wustl.edu>
2016-11-17 12:59:23 -0600

    Create FiveCardDraw.cpp

---
+++ lab3/FiveCardDraw.cpp
@@ -0,0 +1,33 @@
+#include "stdafx.h"
+#include "deck.h"
+#include "functions.h"
+#include "game.h"
+#include "FiveCardDraw.h"
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <random>
+#include <chrono>
+#include <memory>
+
+
+
+FiveCardDraw::FiveCardDraw()
+	: dealer(0), deck(0) {
+	for (int i = 0; i < enum_suit_strings.length; i++) { //variable not accessible HELP CUCKIER
+		for (int j = 0; j < enum_rank_strings.length; j++) {
+			Card c;
+			c.rank = enum_rank_strings.length[j];
+			c.suit = enum_suit_strings[i];
+			deck.add_card(c);
+		}
+	}
+}
+
+int before_turn(player & p) {
+	//add implementation
+}
+
